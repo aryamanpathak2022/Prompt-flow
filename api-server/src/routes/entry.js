4cf4f0cbc5ecb7a1e53bbccf03fe4dc91117ed33
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-09-21 22:57:33 +0530

    Update

--- api-server/src/routes/entry.js
+++ api-server/src/routes/entry.js
@@ -12,7 +12,7 @@ router.post('/create-container', async (req, res) => {
     const rand = Math.floor(Math.random() * 1000);
 
 // Hardcoded values for image and name
-const image =  'python'; // Example image
+const image =  'nikolaik/python-nodejs'; // Example image
 const name = 'prompt-flow-container' +"122"+ rand; // Example container name
     try {
         // Create a new Docker container
@@ -21,13 +21,11 @@ const name = 'prompt-flow-container' +"122"+ rand; // Example container name
             name: name,
             Tty: true,
             HostConfig: {
-                Binds: ['E:/Prompt-flow/api-server/langchain:/app/langchain'], // Mount langchain folder to /app/langchain in the container
+                Binds: ['E:/Prompt-flow/api-server/langchain:/app/langchain'], // Mount langchain folder
             },
-            Cmd: [
-                'sh', '-c',
-                'pip install -r /app/langchain/requirements.txt || true; python3 /app/langchain/main.py'
-            ]
+            cmd: [    'bash', '-c',    'pip install -r /app/langchain/requirements.txt; python3 /app/langchain/main.py; tail -f /dev/null']
         });
+        
         
 
         // Start the container
