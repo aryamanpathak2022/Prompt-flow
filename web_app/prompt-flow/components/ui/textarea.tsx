34589b03ba8ffaae3e8b5d1cbbb8acf8aa95e782
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-09-26 19:35:29 +0530

    Update

--- web_app/prompt-flow/components/ui/textarea.tsx
+++ web_app/prompt-flow/components/ui/textarea.tsx
@@ -13,7 +13,9 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
           "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
+        // eslint-disable-next-line @typescript-eslint/no-empty-interface
         ref={ref}
+        // eslint-disable-next-line @typescript-eslint/no-empty-interface
         {...props}
       />
     )
