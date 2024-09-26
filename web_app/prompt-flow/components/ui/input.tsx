3f219fea8d9674d49123e6bab64b8c217d7b492e
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-09-26 19:45:31 +0530

    Update

--- web_app/prompt-flow/components/ui/input.tsx
+++ web_app/prompt-flow/components/ui/input.tsx
@@ -2,11 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
@@ -15,14 +11,13 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
-        // eslint-disable-next-line @typescript-eslint/no-empty-interface
         ref={ref}
-        // eslint-disable-next-line @typescript-eslint/no-empty-interface
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
 
-export { Input }
+Input.displayName = "Input";
+
+export { Input };
--- web_app/prompt-flow/components/ui/textarea.tsx
+++ web_app/prompt-flow/components/ui/textarea.tsx
@@ -1,12 +1,8 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-export interface TextareaProps
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
-
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+const Textarea = React.forwardRef<HTMLTextAreaElement, React.TextareaHTMLAttributes<HTMLTextAreaElement>>(
   ({ className, ...props }, ref) => {
     return (
       <textarea
@@ -14,14 +10,12 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
           "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
-        // eslint-disable-next-line @typescript-eslint/no-empty-interface
         ref={ref}
-        // eslint-disable-next-line @typescript-eslint/no-empty-interface
         {...props}
       />
-    )
+    );
   }
-)
-Textarea.displayName = "Textarea"
+);
+Textarea.displayName = "Textarea";
 
-export { Textarea }
+export { Textarea };
